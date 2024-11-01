81ac95a72a8a95f35b34db4b1443cfaad789eba8
RedSky <me.redskyll@gmail.com>
2024-11-01 21:38:32 +0330

    some changes

--- Crypto.kt
+++ Crypto.kt
@@ -1,16 +1,21 @@
-package com.example.kotlinbasics
 // this is kotlin version of my cryptography project
 
 const val key = 12 // it should not be Prime
-const val arraySize = 31
-fun main() {   
+val ch = arrayOf(
+    '-', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', ',', ';', 'A',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+)
+val arraySize = ch.size
+
+fun main() {
 
     println()
     println("****--> WELCOME TO MASSAGE ENCODER PROGRAM <--****")
     println("to use app enter one of this numbers: ")
 
     var state = true
-    do { 
+    do {
         println()
         println("\t1 --> encode text")
         println("\t2 --> decode text")
@@ -69,14 +74,14 @@ fun decoder(input: String):String{
 
 fun charEncoder(element: Char, prev: Int, machineCode: Int, arrayLength: Int): Char{
     var x: Long = 0
-    for (i in 0 until arrayLength)
+    for (i in 0..<arrayLength)
         if(i == num(element.uppercaseChar()))
             x = i.toLong()
     return Ch(((x+(machineCode * x) + prev)% arraySize).toInt())
 }
 fun charDecoder(element: Char, past: Int, machineCode: Int, arrayLength: Int): Char{
     var x = 0
-    for (i in 0 until arrayLength){
+    for (i in 0..<arrayLength){
         if((i+machineCode*i+past)%arrayLength == num(element.uppercaseChar())){
             x = i
         }
@@ -85,77 +90,14 @@ fun charDecoder(element: Char, past: Int, machineCode: Int, arrayLength: Int): C
 }
 
 fun num(c: Char): Int{
-    val ch = arrayOfNulls<Char>(arraySize)
-    ch[0] = 'A'
-    ch[1] = 'B'
-    ch[2] = 'C'
-    ch[3] = 'D'
-    ch[4] = 'E'
-    ch[5] = 'F'
-    ch[6] = 'G'
-    ch[7] = 'H'
-    ch[8] = 'I'
-    ch[9] = 'J'
-    ch[10] = 'K'
-    ch[11] = 'L'
-    ch[12] = 'M'
-    ch[13] = 'N'
-    ch[14] = 'O'
-    ch[15] = 'P'
-    ch[16] = 'Q'
-    ch[17] = 'R'
-    ch[18] = 'S'
-    ch[19] = 'T'
-    ch[20] = 'U'
-    ch[21] = 'V'
-    ch[22] = 'W'
-    ch[23] = 'X'
-    ch[24] = 'Y'
-    ch[25] = 'Z'
-    ch[26] = ' '
-    ch[27] = '.'
-    ch[28] = ','
-    ch[29] = ';'
-    ch[30] = '-'
     for (i in ch.indices){
         if(ch[i] == c)
             return i
     }
     return -1
 }
+
 fun Ch(num: Int): Char{
-    val ch = arrayOfNulls<Char>(arraySize)
-    ch[0] = 'A'
-    ch[1] = 'B'
-    ch[2] = 'C'
-    ch[3] = 'D'
-    ch[4] = 'E'
-    ch[5] = 'F'
-    ch[6] = 'G'
-    ch[7] = 'H'
-    ch[8] = 'I'
-    ch[9] = 'J'
-    ch[10] = 'K'
-    ch[11] = 'L'
-    ch[12] = 'M'
-    ch[13] = 'N'
-    ch[14] = 'O'
-    ch[15] = 'P'
-    ch[16] = 'Q'
-    ch[17] = 'R'
-    ch[18] = 'S'
-    ch[19] = 'T'
-    ch[20] = 'U'
-    ch[21] = 'V'
-    ch[22] = 'W'
-    ch[23] = 'X'
-    ch[24] = 'Y'
-    ch[25] = 'Z'
-    ch[26] = ' '
-    ch[27] = '.'
-    ch[28] = ','
-    ch[29] = ';'
-    ch[30] = '-'
     return ch[num]!!
 }
 
